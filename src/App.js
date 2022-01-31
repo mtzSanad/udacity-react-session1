ab21d40c86b7535d960bc77fd47afba007068a0a
mtzsanad <mtz.sanad@gmail.com>
2022-01-31 08:37:39 +0200

    Adding updatable component to UI

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
-import './App.css';
-import ShoppingItem from './component/ShoppingItem';
-
+import "./App.css";
+import ShoppingItem from "./component/ShoppingItem";
 
 const App = () => {
-  const tshirtImage = "https://images.unsplash.com/photo-1521572163474-6864f9cf17ab?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8dCUyMHNoaXJ0fGVufDB8fDB8fA%3D%3D&w=1000&q=80";
-  const jacketImage = "https://media.istockphoto.com/photos/young-handsome-guy-in-a-warm-knitted-clothes-against-a-background-of-picture-id1300962106?k=20&m=1300962106&s=612x612&w=0&h=TkNDPpgEDHYG6xXG-f6x7Gg7dF9EMKQTFpSkbOpIniM=";
+  const tshirtImage =
+    "https://images.unsplash.com/photo-1521572163474-6864f9cf17ab?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8dCUyMHNoaXJ0fGVufDB8fDB8fA%3D%3D&w=1000&q=80";
+  const jacketImage =
+    "https://media.istockphoto.com/photos/young-handsome-guy-in-a-warm-knitted-clothes-against-a-background-of-picture-id1300962106?k=20&m=1300962106&s=612x612&w=0&h=TkNDPpgEDHYG6xXG-f6x7Gg7dF9EMKQTFpSkbOpIniM=";
   return (
-    <div className='container'>
+    <div className="container">
       {/*Now we are passing params to our dumy component*/}
-      <ShoppingItem headerName="T-Shirt2" imgSrc={tshirtImage}/>
+      <ShoppingItem headerName="T-Shirt2" imgSrc={tshirtImage} />
 
       {/*We can create more component*/}
-      <ShoppingItem headerName="Jacket" imgSrc={jacketImage}/>
+      {/*We have to wrap numbers between {}*/}
+      <ShoppingItem headerName="Jacket" imgSrc={jacketImage} itemStock={10} />
     </div>
   );
-}
+};
 
 export default App;
--- src/component/ShoppingItem.js
+++ src/component/ShoppingItem.js
@@ -1,27 +1,56 @@
-import React from 'react';
-import  './ShoppingItem.css'
+import React from "react";
+import "./ShoppingItem.css";
 
 const ShoppingItem = (props) => {
   //Destructuring props object
-  const {headerName, imgSrc} = props;
+  const { headerName, imgSrc } = props;
+  //In case of itemStock was not sent, it will set a default of 5 pcs, we used let as we will change it later
+  let { itemStock = 5 } = props;
+  //Fixed color array
+  const colorArray = ["green", "red", "yellow"];
+  //Enhancing component code readability
+  const colorComponent = colorArray.map((element, key) => (
+    <div key={key} className={`small-box ${element}`}></div>
+  ));
 
-  const colorArray =['green','red','yellow']
+  //Function that is responsible of buying items, which will decrease the stockItem count till zero.
+  //This function will not work correctly, because we are not using the state
+  //Clicking this function will log and modify the itemStock but UI will not be affected! Why?
+  //Because React dosn't know that we have changed the variable and we didn't told react to re-render!
+  //Solution - we must rely on component states where react can listen to changes and update UI accordinally
+  //Check session-1-5- branch
+  const buyItem = (event) => {
+    if (itemStock > 0) {
+      itemStock = itemStock - 1;
+      console.log(itemStock);
+    }
+  };
 
-  return <div className='card'>
-      <header className="card-header">
-        {headerName}
-      </header>
+  return (
+    <div className="card">
+      <header className="card-header">{headerName}</header>
       <section className="card-body">
         <div className="box-container">
           <img className="img" src={imgSrc} alt={headerName} />
-          <div className='desc'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis asperiores et labore numquam modi nemo saepe explicabo maiores exercitationem eveniet?</div>
+          <div className="desc">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis
+            asperiores et labore numquam modi nemo saepe explicabo maiores
+            exercitationem eveniet?
+            <div className="stock">Stock : {itemStock}</div>
+          </div>
         </div>
-        <div className="box-container">
-            {colorArray.map( element => (<div className={`small-box ${element}`}></div>) )}
-        </div>
+        <div className="box-container">{colorComponent}</div>
       </section>
-      <button className='full-button'>Buy</button>
-  </div>;
-}
+      {/** Each react component has normal html event listeners as onListenerName*/}
+      {/** Important Note we don't excute the function here we just set a refrence to it, the actual call will happen on click*/}
+      <button onClick={buyItem} className="full-button">
+        Buy
+      </button>
+      {/** We can re-write button component as following*/}
+      {/** Again we are not excuting the function, we are creating anonymous function that takes the event and pass it to function when called*/}
+      {/**<button onClick={(e)=>{buyItem(e)}} className="full-button">Buy</button>*/}
+    </div>
+  );
+};
 
 export default ShoppingItem;
